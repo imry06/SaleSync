04785d1fb3625510af2181555da2e23f75dfe13d
Rajdev Yadav <rajdevyadavofficial@gmail.com>
2026-06-28 12:47:39 -0400

    dashboard header designed.

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,9 +1,21 @@
 // import React from 'react'
+import "../css/dashboard.css";
+
 
 const Dashboard = () => {
   return (
-    <div>Dashboard</div>
-  )
-}
+    <div className="dashboard">
+      <div className="dashboard-header">
+        <img
+            src="https://intercom.help/scoutmotors/assets/favicon"
+            alt="SaleSync Logo" className="dashboard-logo"
+          />
+          <h3>SaleSync</h3>
+      </div>
+     
+      <hr />
+    </div>
+  );
+};
 
-export default Dashboard
+export default Dashboard;
