0fa8acca055373eefbbe83869022c1afbe4ee060
Rajdev Yadav <rajdevyadavofficial@gmail.com>
2026-06-28 23:37:37 -0400

    search by product input added.

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,42 +1,54 @@
 // import React from 'react'
 import "../css/dashboard.css";
-import {useAuth} from "../context/AuthContext";
+import { useAuth } from "../context/AuthContext";
 import { useNavigate } from "react-router-dom";
+import { useEffect, useState } from "react";
 
 const Dashboard = () => {
-  const {logout} = useAuth();
-const navigate = useNavigate();
+  const { logout } = useAuth();
+  const navigate = useNavigate();
 
   // Function to handle logout
-  const handleLogout = ()=>{
+  const handleLogout = () => {
     logout();
-  }
+  };
 
   // Function to handle navigation to different POS pages
   const handleNavigation = (path) => {
     navigate(path);
   };
 
+  // function to handle date-time display
+
   return (
     <div className="dashboard">
       <div className="dashboard-header">
         <img
-            src="https://intercom.help/scoutmotors/assets/favicon"
-            alt="SaleSync Logo" className="dashboard-logo"
-          />
-          <h2>aleSync</h2>
+          src="https://intercom.help/scoutmotors/assets/favicon"
+          alt="SaleSync Logo"
+          className="dashboard-logo"
+        />
+        <h2>aleSync</h2>
       </div>
-     
-     
-     <div className="dashboard-body">
 
-      <div className="dashboard-buttons">
-        <button onClick={() => handleNavigation("/sale-pos")}>Sale POS</button>
-        <button onClick={() => handleNavigation("/rubic-pos")}>Rubic POS</button>
-        <button onClick={() => handleNavigation("/purchase-pos")}>Purchase POS</button>
-        <button onClick={handleLogout}><i className="fa-solid fa-arrow-right-from-bracket"></i> LogOut</button>
+      <div className="dashboard-body">
+        <div className="dashboard-buttons">
+          <button onClick={() => handleNavigation("/sale-pos")}>
+            Sale POS
+          </button>
+          <button onClick={() => handleNavigation("/rubic-pos")}>
+            Rubic POS
+          </button>
+          <button onClick={() => handleNavigation("/purchase-pos")}>
+            Purchase POS
+          </button>
+          <button onClick={handleLogout}>
+            <i className="fa-solid fa-arrow-right-from-bracket"></i> LogOut
+          </button>
+        </div>
+
+        <h1></h1>
       </div>
-     </div>
     </div>
   );
 };
--- src/pages/RubicPOS.jsx
+++ src/pages/RubicPOS.jsx
@@ -1,9 +1,21 @@
-import React from 'react';
+import React from "react";
 
 const RubicPOS = () => {
   return (
-    <div>RubicPOS</div>
-  )
-}
+    <div className="rubic-pos">
+      <div className="rubic-pos-header">
+        <form action="">
+          <label htmlFor="by-product">By Product : </label>
+          <input
+            type="text"
+            name="by-product"
+            id="by-product"
+            placeholder="Enter product name"
+          />
+        </form>
+      </div>
+    </div>
+  );
+};
 
 export default RubicPOS;
